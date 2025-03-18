e12aa6e6a38dee4b5043c2c669004d6921ae9d22
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-03-18 14:16:46 -0300

    funções para manipular arquivos com xlwings

--- SRC/excel_file.py
+++ SRC/excel_file.py
@@ -14,3 +14,18 @@ class excel_file:
             self.path = None
             self.visibility = visibility
             self.filtered = filtered
+
+    #file manipulation
+
+    def new_workbook(self,directory,name):
+        file_path = os.path.join(directory,name)
+        self.save_workbook(file_path)
+
+    def close_workbook(self):
+        self.book.close()
+    
+    def save_workbook(self,path):
+        self.book.save(path)
+
+    def quit_whitout_saving(self):
+        self.book.app.quit()
