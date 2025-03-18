5d8e4408026bcbf65ab83e48dd1233ce07c9c74a
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-03-18 14:14:34 -0300

    inicio da classe excel_file

---
+++ SRC/excel_file.py
@@ -0,0 +1,16 @@
+import xlwings as xw
+import os 
+import pandas as pd
+
+class excel_file:
+    def __init__(self,path = None,visibility = True, filtered = False):
+        if path:
+            self.path = path
+            self.visibility = visibility
+            self.filtered = filtered
+            self.book = xw.Book(self.path)
+        else:
+            self.book = xw.Book()
+            self.path = None
+            self.visibility = visibility
+            self.filtered = filtered
