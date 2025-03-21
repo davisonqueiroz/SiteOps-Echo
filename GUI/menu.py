478dd0a43177de4ecc842705e02c04d35e456e72
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-03-21 16:24:13 -0300

    criação da pasta de icons e adição dos mesmos

--- GUI/menu.py
+++ GUI/menu.py
@@ -1,5 +1,6 @@
 from default_window import window
 import customtkinter as ctk
+from PIL import Image, ImageTk
 
 class menu_window(window):
 
@@ -10,6 +11,7 @@ class menu_window(window):
         self.window.geometry(f'{width}x{height}+{posx}+{posy}')
         base_colour = "#EEEEEE"
         secondary_colour = "#304FFE"
+        menu_icon = ctk.CTkImage(Image.open("ICONS/home_icon.png"),size=(30,30))
 
     def create_button(self, name_btn, text, height, width, pos_horz, pos_vert, font=..., command=None, fg_color="gray",text_color = "white",bg_color ="white", position_type="place",corner_radius = 0,image = None,border_width= 0,hover_color = "white"):
         super().create_button(name_btn, text, height, width, pos_horz, pos_vert, font, command, fg_color,text_color,bg_color,position_type,corner_radius,image,border_width,hover_color)
@@ -20,5 +22,6 @@ class menu_window(window):
         menu_bar_frame.pack_propagate(False)
         menu_bar_frame.configure(width=45)
 
-        self.create_button("menu_btn","",30,30,4,10,bg_color=self.secondary_color,hover_color=self.secondary_color)
+        self.create_button("menu_btn","",30,30,4,10,bg_color=self.secondary_color,hover_color=self.secondary_color,image=self.menu_icon)
+        
 
