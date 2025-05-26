e859d1df7ee1840c75b59af3b93db9adde29327c
Bpelossi <brunopelossi@gmail.com>
2025-05-26 14:45:24 -0300

    Criação da GUI para utilities

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -4,6 +4,7 @@ from GUI.content_area import *
 from GUI.menu_bar import *
 from GUI.view.home import *
 from GUI.view.cruzeiro import *
+from GUI.view.utilities import *
 
 class MainWindow(QMainWindow):
     def __init__(self):      
@@ -19,13 +20,15 @@ class MainWindow(QMainWindow):
 
         self.home = Home()
         self.cruzeiro = CruzeiroDoSul()
+        self.utilites = Utilities()
 
 
         #cria e seta o menu_bar
         self.menu_bar = MenuBar("#FF7E29",97,self.main_layout,"#F5F5F5","#FF7E29",33,"#FA9653","#F5F5F5")
         self.menu_bar.create_menu_button("#FF7E29","#FA9653","ASSETS/ICONS/menu_icon.png")
-        self.home_btn = self.menu_bar.add_sub_menu_button("        home","ASSETS/ICONS/home_icon.png")
+        self.home_btn = self.menu_bar.add_sub_menu_button("        Home","ASSETS/ICONS/home_icon.png")
         self.config = self.menu_bar.add_sub_menu_button("        Cruzeiro","ASSETS/ICONS/cruzeiro_icon.png")
+        self.utilites_btn = self.menu_bar.add_sub_menu_button("        Utilities","ASSETS/ICONS/utilities_icon.png")
         self.menu_bar.start_selected_button(self.home_btn)
         self.buttons = self.menu_bar.get_sub_menu_buttons()
 
@@ -35,11 +38,13 @@ class MainWindow(QMainWindow):
 
         self.stacks.addWidget(self.home)
         self.stacks.addWidget(self.cruzeiro)
+        self.stacks.addWidget(self.utilites)
 
         self.main_layout.addWidget(self.central_frame)
 
         self.menu_bar.set_action(self.home_btn,lambda: self.stacks.setCurrentIndex(0))
         self.menu_bar.set_action(self.config,lambda: self.stacks.setCurrentIndex(1))
+        self.menu_bar.set_action(self.utilites_btn,lambda: self.stacks.setCurrentIndex(2))
 
         self.show()
 
