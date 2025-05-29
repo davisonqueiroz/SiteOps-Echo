509d73419cd3ee578e0d439e0a03a3b53d7b0a48
Bpelossi <brunopelossi@gmail.com>
2025-05-29 17:03:51 -0300

    vinculação das funções do utilities com o GUI

--- GUI/view/utilities.py
+++ GUI/view/utilities.py
@@ -3,6 +3,8 @@ from GUI.widgets.cards import *
 from MODELS.Cruzeiro_do_Sul.pos_grad_ead import *
 from MODELS.Cruzeiro_do_Sul.tecnico import *
 from MODELS.Utilities.duplicates import *
+from MODELS.Utilities.csv_converter import *
+from MODELS.Utilities.fix_cities import *
 from MODELS.Utilities.divisor import *
 
 
@@ -20,25 +22,25 @@ class Utilities(ContentArea):
 
         self.card_exp_msp = Card("exp_msp","#FF7E29")
         self.card_exp_msp.create_front_card("Exp para MSP","#FF7E29","#F5F5F5","#000000","#D4D4D4","#8148C9","#F5F5F5","#7D3FC9","Selecione a planilha")
-        self.card_exp_msp.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha MSP. \n2. Selecione o EXP de campus.\n3.Clique em 'Gerar'\n\n*IMPORTANTE: Certifique-se de selecionar na ordem correta, conforme instruções*")
+        self.card_exp_msp.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha EXP. \n2.Clique em 'Gerar'\n\nIMPORTANTE: O arquivo será salvo na pasta original com 'MSP_' na frente do nome")
         self.card_exp_msp.set_action_btn("btn_generate", self.process_files)   
         
         self.card_duplicates = Card("duplicates","#FF7E29")
         self.card_duplicates.create_front_card("Remover Duplicadas","#FF7E29","#F5F5F5","#000000","#D4D4D4","#8148C9","#F5F5F5","#7D3FC9","Selecione a planilha")
-        self.card_duplicates.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha MSP. \n2. Selecione o EXP de campus.\n3.Clique em 'Gerar'\n\n*IMPORTANTE: Certifique-se de selecionar na ordem correta, conforme instruções*")
+        self.card_duplicates.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione o modelo da planilha. \n2. Selecione a planilha no modelo correto.\n3.Clique em 'Gerar'\n\nIMPORTANTE: O arquivo é salvo na pasta original com 'no_dup_' na frente do nome")
         cb_box_duplicates = self.card_duplicates.create_combobox(["MSP", "EXP"])
         self.card_duplicates.add_component_card(cb_box_duplicates,"combobox")
         self.card_duplicates.set_action_btn("btn_generate", self.process_dup)
         
         self.card_csv = Card("csvtoxlsx","#FF7E29")
         self.card_csv.create_front_card("CSV para Excel","#FF7E29","#F5F5F5","#000000","#D4D4D4","#8148C9","#F5F5F5","#7D3FC9","Selecione a planilha")
-        self.card_csv.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha MSP. \n2. Selecione o EXP de campus.\n3.Clique em 'Gerar'\n\n*IMPORTANTE: Certifique-se de selecionar na ordem correta, conforme instruções*")
-        self.card_csv.set_action_btn("btn_generate", self.process_files)
+        self.card_csv.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha CSV. \n2.Clique em 'Gerar'\n\nIMPORTANTE: O arquivo será salvo na pasta original com 'xlsx_' na frente do nome")
+        self.card_csv.set_action_btn("btn_generate", self.process_csv_converter)
 
         self.card_fix_cities = Card("fix_cities","#FF7E29")
         self.card_fix_cities.create_front_card("Corrigir Cidades","#FF7E29","#F5F5F5","#000000","#D4D4D4","#8148C9","#F5F5F5","#7D3FC9","Selecione a planilha")
-        self.card_fix_cities.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha MSP. \n2. Selecione o EXP de campus.\n3.Clique em 'Gerar'\n\n*IMPORTANTE: Certifique-se de selecionar na ordem correta, conforme instruções*")
-        self.card_fix_cities.set_action_btn("btn_generate", self.process_files)        
+        self.card_fix_cities.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha MSP de Campus. \n2.Clique em 'Gerar'\n\nIMPORTANTE: O arquivo será salvo na pasta original com 'fixed_cities_' na frente do nome")
+        self.card_fix_cities.set_action_btn("btn_generate", self.process_fix_cities)        
         
         self.add_card(self.card_exp_msp,"TOP")
         self.add_card(self.card_duplicates,"TOP")
@@ -58,16 +60,26 @@ class Utilities(ContentArea):
                 self.card_tecnico.set_text_btns(["btn_option1","btn_option2"],"Selecione o arquivo")
 
     def process_dup(self):
-        print('entrou no process')
         selected = self.card_duplicates.get_selected_text()
         path = self.card_duplicates.paths["btn_option1"]
-        print(selected)
-        print(path)
         if path and selected:
-            print('Entrou if')
             dup = RemoverDuplicadas(path,selected)
             dup.remover()
             self.card_duplicates.set_text_btns(["btn_option1"])
+
+    def process_csv_converter(self):
+        path = self.card_csv.paths["btn_option1"]
+        if path:
+            csv_converter = CSVConverter(path)
+            csv_converter.converter_para_excel()
+            self.card_csv.set_text_btns(["btn_option1"])
+
+    def process_fix_cities(self):
+        path = self.card_fix_cities.paths["btn_option1"]
+        if path:
+            fix_cities = CorrigirCidades(path)
+            fix_cities.executar()
+            self.card_fix_cities.set_text_btns(["btn_option1"])
 
     
     def create_division(self):
