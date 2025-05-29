6cd19674ae8268aa28384e11bbde22f3feef41cf
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-05-29 10:57:04 -0300

    adição do metodo para dividir tabelas e modificações necessarias para uso

--- GUI/view/utilities.py
+++ GUI/view/utilities.py
@@ -2,12 +2,22 @@ from GUI.content_area import *
 from GUI.widgets.cards import *
 from MODELS.Cruzeiro_do_Sul.pos_grad_ead import *
 from MODELS.Cruzeiro_do_Sul.tecnico import *
+from MODELS.Utilities.divisor import *
 
 
 class Utilities(ContentArea):
     def __init__(self):
         super().__init__("#F5F5F5","VBox")
         self.set_cards_area("#F5F5F5",self.content)
+
+        self.card_divisor = Card("divisor_table","#FF7E29")
+        self.card_divisor.create_front_card("Dividir tabela","#FF7E29","#F5F5F5","#000000","#D4D4D4","#8148C9","#F5F5F5","#7D3FC9","Selecione a planilha")
+        self.card_divisor.create_back_card("#FF7E29","1.Selecione em quantas tabelas deseja dividir a planilhazn2.Selecione o arquivo.\n3.Clique em 'Gerar'\n4.Selecione onde deseja salvar(todos os arquivos serão gerados no diretório selecionado)")
+        cb_box_divisor = self.card_divisor.create_combobox(["2","3","4","5","6","7","8"])
+        self.card_divisor.add_component_card(cb_box_divisor,"combobox")
+        self.card_divisor.set_action_btn("btn_generate", self.create_division)
+
+        
 
         self.card_exp_msp = Card("exp_msp","#FF7E29")
         self.card_exp_msp.create_front_card("Exp para MSP","#FF7E29","#F5F5F5","#000000","#D4D4D4","#8148C9","#F5F5F5","#7D3FC9")
@@ -33,6 +43,7 @@ class Utilities(ContentArea):
         self.add_card(self.card_duplicates,"TOP")
         self.add_card(self.card_csv,"TOP")
         self.add_card(self.card_fix_cities,"BOTTOM")
+        self.add_card(self.card_divisor,"BOTTOM")
 
 
     def process_files(self):
@@ -44,3 +55,15 @@ class Utilities(ContentArea):
                 path_save = self.card_tecnico.paths["save"]
                 tecnico.saving_and_separing_pendings(path_save)
                 self.card_tecnico.set_text_btns(["btn_option1","btn_option2"],"Selecione o arquivo")
+    
+    def create_division(self):
+        selected = self.card_divisor.get_selected_text()
+        if self.card_divisor.paths["btn_option1"] and selected:
+             file = self.card_divisor.paths["btn_option1"]
+             div = TableDivisor(file,selected)
+             self.card_divisor.set_directory("btn_generate")
+             path_save = self.card_divisor.paths["save"]
+             div.create_files(path_save)
+             self.card_divisor.set_text_btns(["btn_option1"])
+
+         
---
+++ MODELS/Utilities/divisor.py
@@ -0,0 +1,21 @@
+import MODELS.excel_file as ef
+import os
+class TableDivisor:
+    def __init__(self,excel_file,quantity_divisions):
+        self.excel_file = excel_file
+        self.quantity_divisions = int(quantity_divisions)
+        self.calc_quantity_rows()
+
+    def calc_quantity_rows(self):
+        self.table = ef.new_dataframe(self.excel_file,0)
+
+        total_rows = self.table.iloc[:, 1].last_valid_index() + 1
+        self.rows_for_division = total_rows // self.quantity_divisions
+
+    def create_files(self,path):
+        name_file = os.path.splitext(os.path.basename(self.excel_file))[0]
+        for i in range(1,self.quantity_divisions):
+            df_temp = ef.get_rows(self.table,range(0,self.rows_for_division))
+            ef.save_df(os.path.join(path,f"{name_file}{i}.xlsx"),df_temp,"Sheet 1")
+            self.table = ef.remove_rows(self.table,range(0,self.rows_for_division))
+        ef.save_df(os.path.join(path,f"{name_file}{self.quantity_divisions}.xlsx"),self.table,"Sheet 1")
