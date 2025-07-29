9dc2bd97993a979f0cb526dda676aac595c65ae1
davison queiroz <davisonvinicius@outlook.com.br>
2025-07-29 14:09:27 -0300

    refatoração da classe pos grad para que use sheetmanip e dfutils

--- MODELS/Cruzeiro_do_Sul/pos_grad_ead.py
+++ MODELS/Cruzeiro_do_Sul/pos_grad_ead.py
@@ -1,123 +1,149 @@
-import MODELS.excel_file as ef
+from MODELS.excel_file.SheetManipulation import SheetManipulation as sma
+from MODELS.excel_file.DataFrameUtils import DataFrameUtils as dfu
 import pandas as pd
 import openpyxl
 import os
+from datetime import date
 class pos_grad_ead:
-    def __init__(self,file_msp,file_campus,file_relation_campus):
-        file_msp = file_msp
-        file_campus = file_campus
-        file_relation_campus = file_relation_campus
+    def __init__(self,offer_file,file_campus,file_ratio_campus):
+        self.headers_transform = {
+            " CÓDIGO DA IES" : "Nome da IES",  
+            "CURSO" : "Nome do Curso",
+            "METODOLOGIA" : "Modalidade",
+            "DURAÇÃO (MESES)" : "Duração do Curso",
+            "QUANTD PARCELAS" : "Quantidade de Parcelas",
+            "PREÇO PARCELAS" : "Mensalidade sem desconto",
+            "PORCENTAGEM DE DESCONTO" : "Porcentagem de desconto da bolsa (Fixo/1 º Semestre)",
+        }   
+        self.offers = offer_file
+        self.campus = file_campus
+        self.campus_ratio = file_ratio_campus
 
-        self.sheet_process(file_msp,file_campus,file_relation_campus)
-        self.lookup_ids()
-        self.remove_repetitions()
-        self.concat_and_join()
-        self.concat_campus()
-        self.separing_universities_msp()
+    def _load_dataframes(self):
+        self.offers = sma(self.offers).convert_to_msp_offers_and_load(self.headers_transform)
+        self.campus = sma(self.campus).load()
+        self.pole_ratio_unp = sma(self.campus_ratio,"UNIPÊ").load()
+        self.pole_ratio_pos = sma(self.campus_ratio,"POSITIVO").load()
 
-    def sheet_process(self,file_msp,file_exp,file_relation):
+    def _adjusts_in_msp(self):
+        self.msp_columns_adjusts()
+        self.transform_and_subtract_value()
+        self.separe_universities_courses()
 
-        self.relation_pending,self.course_pendings,self.campus_pending =  None, None, None
-        dtype_campus = {
-            'id' : str,
-            'metadata_code' : str,
-            'university_id' : str
-        }
-        dtype_msp = {
-            'ID_POLO' : str,
-            'COD_CURSO' : str,
-            'Semestre de Ingresso': str,
-        }
-        dtype_relation = {
-            'COD_POLO' : str
-        }
-        self.msp = ef.new_dataframe(file_msp,"Modelo Sem Parar",dtype= dtype_msp)
-        self.relation_unipe = ef.new_dataframe(file_relation,"UNIPÊ",dtype= dtype_relation)
-        self.relation_positivo = ef.new_dataframe(file_relation,"POSITIVO",dtype= dtype_relation)
-        self.exp_cruzeiro = ef.new_dataframe(file_exp,"Sheet 1",dtype= dtype_campus)
-        if ef.verify_nas(self.exp_cruzeiro['metadata_code']):
-            self.campus_pending = ef.get_nulls_in_dataframe(self.exp_cruzeiro,'metadata_code')
-            self.exp_cruzeiro = ef.remove_nas_dataframe(self.exp_cruzeiro,'metadata_code')
-        self.exp_unipe = ef.new_filtered_dataframe(self.exp_cruzeiro,"1593","university_id")
-        self.exp_positivo = ef.new_filtered_dataframe(self.exp_cruzeiro,"1639","university_id")
-        self.exp_cruzeiro = ef.new_filtered_dataframe(self.exp_cruzeiro,"3719","university_id")
+    def _adjusts_in_campus(self):
+        self.remove_campus_have_nulls()
+        self.separate_campus()
+        self.lookup_campus_ids()
+        self.remove_campus_that_repeat()
 
-    def lookup_ids(self):
+    def _fill_campus_id_in_msps(self):
+        self.concat_values_in_ratio_and_join()
+        self.create_concat_campus()
+        self.fill_in_campus_id_msp()
+        self.concat_ies_offers()
 
-        ef.xlookup_pd(self.relation_unipe,self.exp_unipe,'COD_POLO','metadata_code','id','campus_id')
-        ef.xlookup_pd(self.relation_positivo,self.exp_positivo,'COD_POLO','metadata_code','id','campus_id')
-        if ef.verify_nas(self.relation_positivo['campus_id']):
-            self.relation_pending = ef.get_nulls_in_dataframe(self.relation_positivo,'campus_id')
-            self.relation_positivo = ef.remove_nas_dataframe(self.relation_positivo,'campus_id')
-        if ef.verify_nas(self.relation_unipe['campus_id']):
-            self.relation_pending = ef.get_nulls_in_dataframe(self.relation_unipe,'campus_id')
-            self.relation_unipe = ef.remove_nas_dataframe(self.relation_unipe,'campus_id')
+    def msp_columns_adjusts(self):
+        self.offers['Turno'] = "Virtual"
+        self.offers['Tipo de duração do curso'] = "mes"
+        self.offers['Grau'] = "Especialização (pós-graduação)"
+        self.offers['Qual valor usar?\n% ou R$'] = "porcentagem"
+        self.offers['LIMTADA?'] = "FALSE"
+        self.offers['Data de Início da Oferta?'] = self._get_date_actually()
+        self.offers['Porcentagem de desconto IES'] = self._transform_and_subtract_value(self.offers,'Porcentagem de desconto da bolsa (Fixo/1 º Semestre)')
+        self.offers['ID da IES'] = dfu.find_and_replace(self.offers,'Nome da IES','CRUZEIRO DO SUL - PÓS EAD','ID da IES',3719)
+        self.offers['ID da IES'] = dfu.find_and_replace(self.offers,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID da IES',1639)
+        self.offers['ID da IES'] = dfu.find_and_replace(self.offers,'Nome da IES','UNIPÊ - PÓS-GRADUAÇÃO EAD','ID da IES',1593)
+        #TODO: Pegar data end, OSC e semestre de ingresso por formulário
+
+    def get_date_actually(self):
+        today = date.now()
+        return today.strftime("%d%m%Y")
+    
+    def transform_and_subtract_value(self,dataframe,column_name):
+        dataframe[column_name] = dataframe[column_name].astype(float)
+        dataframe[column_name] = dataframe[column_name] - 0.05
+        return dataframe[column_name].round(2)
+
+    def separe_universities_courses(self):
+        self.unipe = dfu.filter_content_by_column(self.offers,'UNIPÊ - PÓS-GRADUAÇÃO EAD','ID da IES')
+        self.positivo = dfu.filter_content_by_column(self.offers,'POSITIVO - PÓS-GRADUAÇÃO EAD','ID da IES')
+        self.cruzeiro = dfu.filter_content_by_column(self.offers,'CRUZEIRO DO SUL - PÓS EAD','ID da IES')
+
+    def remove_campus_have_nulls(self):
+        if dfu.verify_if_have_nulls(self.campus['metadata_code']):
+            self.pending_campus = dfu.get_rows_have_nulls(self.campus,'metadata_code')
+            self.campus = dfu.dropna_rows_by_column(self.campus,'metadata_code')
         
+    def separate_campus(self):
+        self.exp_unp = dfu.filter_content_by_column(self.campus,"1593","university_id")
+        self.exp_pos = dfu.filter_content_by_column(self.campus,"1639","university_id")
+        self.exp_cruzeiro = dfu.filter_content_by_column(self.campus,"3719","university_id")
+    
+    def lookup_campus_ids(self):
+        self.pole_ratio_unp = dfu.xlookup(self.pole_ratio_unp,self.exp_unp,'COD_POLO','metadata_code','id','campus_id')
+        self.pole_ratio_pos = dfu.xlookup(self.pole_ratio_pos,self.exp_pos,'COD_POLO','metadata_code','id','campus_id')
+        if dfu.verify_if_have_nulls(self.pole_ratio_unp['campus_id']):
+            self.pending_unp = dfu.get_rows_have_nulls(self.pole_ratio_unp,'campus_id')
+            self.pole_ratio_unp = dfu.dropna_rows_by_column(self.pole_ratio_unp,'campus_id')
+        if dfu.verify_if_have_nulls(self.pole_ratio_pos['campus_id']):
+            self.pending_pos = dfu.get_rows_have_nulls(self.pole_ratio_pos,'campus_id')
+            self.pole_ratio_pos = dfu.dropna_rows_by_column(self.pole_ratio_pos,'campus_id')
+            self.campus_ratio_pending = dfu.concat_dataframes(self.pending_pos,self.pending_unp)
 
-    def remove_repetitions(self):
-        self.exp_cruzeiro = ef.remove_specify_from_df(self.relation_positivo['COD_POLO'],self.exp_cruzeiro,'metadata_code')
-        self.exp_cruzeiro = ef.remove_specify_from_df(self.relation_unipe['COD_POLO'],self.exp_cruzeiro,'metadata_code')
+    def remove_campus_that_repeat(self):
+        self.exp_cruzeiro = dfu.remove_values_from_column(self.exp_cruzeiro,'metadata_code',self.pole_ratio_pos['COD_POLO'])
+        self.exp_cruzeiro = dfu.remove_values_from_column(self.exp_cruzeiro,'metadata_code',self.pole_ratio_unp['COD_POLO'])
 
-    def concat_and_join(self):
-        self.relation_positivo['concat'] = ef.concat_pd([self.relation_positivo['campus_id'],self.relation_positivo['COD_POLO']],';campus_code:')
-        self.relation_unipe['concat'] = ef.concat_pd([self.relation_unipe['campus_id'],self.relation_unipe['COD_POLO']],';campus_code:')
+    def concat_values_in_ratio_and_join(self):
+        self.pole_ratio_unp['concat'] = dfu.concat_series_with_separator([self.pole_ratio_unp['campus_id'],self.pole_ratio_unp['COD_POLO']],';campus_code:')
+        self.pole_ratio_pos['concat'] = dfu.concat_series_with_separator([self.pole_ratio_pos['campus_id'],self.pole_ratio_pos['COD_POLO']],';campus_code:')
+        self.join_unp = dfu.textjoin_unique(self.pole_ratio_unp['concat'])
+        self.join_pos = dfu.textjoin_unique(self.pole_ratio_pos['concat'])
 
-        self.textjoin_pos = ef.textjoin_pd(self.relation_positivo['concat'])
-        self.textjoin_unipe = ef.textjoin_pd(self.relation_unipe['concat'])
-
-
-    def concat_campus(self):
-        
-        keys = ef.concat_pd([self.exp_cruzeiro['id'],self.exp_cruzeiro['metadata_code']],';campus_code:')
+    def create_concat_campus(self):
+        keys = dfu.concat_series_with_separator([self.exp_cruzeiro['id'],self.exp_cruzeiro['metadata_code']],';campus_code:')
         keys_count = keys.str.len()
         limit_cells = 32767
         sum_cells = 0
         self.groups = []
         init = 0
-
-        for row in range(len(keys)):
+        for row in range(keys):
             current_length = keys_count.iloc[row] + 1
             sum_cells += current_length
             if limit_cells < sum_cells:
-                result = ef.textjoin_pd(keys.iloc[init:row])
+                result = dfu.textjoin_unique(keys.iloc[init:row])
                 self.groups.append(result)
                 init = row
                 sum_cells = keys_count.iloc[row] 
-
         if init < len(keys):
-            result = ef.textjoin_pd(keys.iloc[init:])
+            result = dfu.textjoin_unique(keys.iloc[init:])
             self.groups.append(result)
 
-    def separing_universities_msp(self):
-        self.msp_cruzeiro = ef.new_filtered_dataframe(self.msp,"CRUZEIRO DO SUL - PÓS EAD",'Nome da IES')
-        self.msp = ef.remove_specify_from_df(self.msp_cruzeiro['Nome da IES'],self.msp,'Nome da IES')
-        self.msp = ef.associate_value_from(self.msp,'Nome da IES','UNIPÊ - PÓS-GRADUAÇÃO EAD','ID do Campus',self.textjoin_unipe)
-        if len(self.relation_positivo ) == 1:
-            self.campus_positivo = self.relation_positivo['campus_id'][0]   
-            self.msp = ef.associate_value_from(self.msp,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','COD CAMPUS',self.textjoin_pos)
-            self.msp = ef.associate_value_from(self.msp,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID do Campus',self.campus_positivo)
+    def fill_in_campus_id_msp(self):
+        self.unipe = dfu.find_and_replace(self.unipe,'Nome da IES','UNIPÊ - PÓS-GRADUAÇÃO EAD','ID do Campus',self.join_unp)
+        if len(self.pole_ratio_pos) == 1:
+            self.campus_pos = self.pole_ratio_pos['campus_id'][0]   
+            self.positivo = dfu.find_and_replace(self.positivo,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','COD CAMPUS',self.join_pos)
+            self.positivo = dfu.find_and_replace(self.positivo,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID do Campus',self.campus_pos)
         else:
-            self.msp = ef.associate_value_from(self.msp,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID do Campus',self.textjoin_pos)
+            self.positivo = dfu.find_and_replace(self.positivo,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID do Campus',self.join_pos)
+    
+    def concat_ies_offers(self):
+        self.msp = dfu.concat_dataframes(self.positivo,self.unipe)
 
-    def create_files_limited(self,path):
+    def create_files_and_save(self,file_save):
         range_for = len(self.groups)
-        form = lambda x: f"{x:.2f}"
-        columns = ['Porcentagem de desconto da bolsa (Fixo/1 º Semestre)', 'Porcentagem de desconto IES']
-        self.msp_cruzeiro.update({
-            col: self.msp_cruzeiro[col].apply(form) 
-            for col in columns
-            if col in self.msp_cruzeiro.columns
-        })
-        self.msp.update({
-            col: self.msp[col].apply(form) 
-            for col in columns
-            if col in self.msp.columns
-        })
-
         for i in range(range_for):
-            self.df_temp = ef.associate_value_from(self.msp_cruzeiro,'Nome da IES','CRUZEIRO DO SUL - PÓS EAD',"ID do Campus",self.groups[i])
+            self.df_temp = dfu.find_and_replace(self.cruzeiro,'Nome da IES','CRUZEIRO DO SUL - PÓS EAD',"ID do Campus",self.groups[i])
+            dfu.save_dataframe(os.path.join(file_save,f"CRUZEIRO_POS_GRAD{i}.xlsx"),self.df_temp,'Modelo Sem Parar')
+        dfu.save_dataframe(os.path.join(file_save, "UNIPE_E_POSITIVO.xlsx"),self.msp,'Modelo Sem Parar')
 
-            ef.save_df(os.path.join(path,f"CRUZEIRO_POS_GRAD{i}.xlsx"),self.df_temp,'Modelo Sem Parar')
-        ef.save_df(os.path.join(path, "UNIPE_E_POSITIVO.xlsx"),self.msp,'Modelo Sem Parar')
-
-
+    def load(self,file_to_save):
+        self._load_dataframes()
+        self._adjusts_in_msp()
+        self._adjusts_in_campus()
+        self._fill_campus_id_in_msps()
+        try:
+            self.create_files_and_save(file_to_save)
+        except Exception as e:
+            print (f"Erro durante o processamento: {e}")
