66df781b8519e44b35892c86def28018008d07b9
Bpelossi <brunopelossi@gmail.com>
2025-07-30 16:34:30 -0300

    refactor: streamline RemoverDuplicadas initialization and column comparison logic

--- MODELS/Utilities/duplicates.py
+++ MODELS/Utilities/duplicates.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import os
+from MODELS.excel_file.SheetManipulation import SheetManipulation as sma
+from MODELS.excel_file.DataFrameUtils import DataFrameUtils as dfu
 from GUI.widgets.notifications import Notification 
 
 class RemoverDuplicadas:
-    def __init__(self, excel_file, type_table):
+    def __init__(self, excel_file):
         self.excel_file = excel_file
-        self.type_table = type_table
-        self.df = None
+        self.sheet = sma(self.excel_file)
+        self.df = self.sheet.load()
+        self.type_table = self.sheet.get_sheet_type()
         self.df_no_dup = None
         self.df_dup = None
         self.no_dup_file = None
 
-    def remover(self):
-        if not self.excel_file:
-            Notification.error("Nenhum arquivo selecionado","Selicionar um arquivo")
-        
-        try:
-            # Gera o caminho de saída com base no nome do arquivo original
-            base_nome = os.path.splitext(os.path.basename(self.excel_file))[0]
-            self.no_dup_file = os.path.join(os.path.dirname(self.excel_file), f"No_dup_{base_nome}.xlsx")
-
-            # Lê a planilha de entrada
-            self.df = pd.read_excel(self.excel_file, engine='openpyxl')
-
-            for col in self.df.select_dtypes(include=["object"]).columns:
-                self.df[col] = self.df[col].str.replace(r"http\S+", "", regex=True)
-
-            colunas_exp = [
+    def set_columns_to_compare(self):
+        if self.type_table == "msp_offers":
+            return [
+                "ID do Campus",
+                "Nome do Curso",
+                "Grau",
+                "Modalidade",
+                "Turno",
+                "Duração do Curso",
+                "Mensalidade sem desconto"
+            ]
+        elif self.type_table == "exp_offers":
+            return [
                 "university_id",
                 "campus_id", 
                 "name_from_university",
@@ -37,51 +37,35 @@ class RemoverDuplicadas:
                 "max_payments",
                 "full_price"
             ]
+        else:
+            raise ValueError("Tipo de tabela não reconhecido")
 
-            colunas_msp = [
-                "ID do Campus",
-                "Nome do Curso",
-                "Grau",
-                "Modalidade",
-                "Turno",
-                "Duração do Curso",
-                "Mensalidade sem desconto"
-            ]
+    def execute(self):
+        print(self.type_table)
 
-            if self.type_table == "MSP":
-                colunas_para_comparacao = colunas_msp
-            elif self.type_table == "EXP":
-                colunas_para_comparacao = colunas_exp
-            else:
-                Notification.error("Tipo de tabela não reconhecida","Verificar o modelo da tabela")
-                return
+        # Gera o caminho de saída com base no nome do arquivo original
+        base_nome = os.path.splitext(os.path.basename(self.excel_file))[0]
+        self.no_dup_file = os.path.join(os.path.dirname(self.excel_file), f"No_dup_{base_nome}.xlsx")
 
-            # Cria a coluna de chave única concatenada
-            self.df['SKU'] = self.df[colunas_para_comparacao].astype(str).apply(
-                lambda row: '_'.join(row.values), axis=1
+        print('1------------')
+        print(self.no_dup_file)
+
+        colunas_para_comparacao = self.set_columns_to_compare()
+
+        print('2------------')
+        self.df_no_dup= dfu.remove_duplicates_by_columns(self.df,colunas_para_comparacao)
+        print('3------------')
+        self.df_dup = dfu.get_duplicates_by_column(self.df,colunas_para_comparacao)
+        print('4------------')
+        self.save()
+        print('5------------')  
+
+    def save(self):
+        try:
+            dfu.save_multiple_dataframes(
+                [self.df_no_dup, self.df_dup],
+                self.no_dup_file,
+                ["Sem Duplicadas", "Duplicadas"]
             )
-
-            # Cria o df csó com as linhas duplicatas
-            self.df_dup= self.df[self.df.duplicated(subset='SKU', keep=False)]
-
-            # Cria um novo df removendo as duplicatas 
-            self.df_no_dup = self.df.drop_duplicates(subset='SKU', keep='first')
-
-            # Remove a coluna auxiliar da tabela modelo 'exp'
-            if self.type_table =='exp':
-                self.df_no_dup = self.df_no_dup.drop(columns=['SKU'])
-                self.df_dup = self.df_dup.drop(columns=['SKU'])
-
-            # Salvar o Df sem duplicadas na tabela original e cria uma nova aba 'Duplicados' para salvar os valores Duplicados
-            try:
-                with pd.ExcelWriter(self.no_dup_file, engine='openpyxl') as writer:
-                    self.df_no_dup.to_excel(writer, index=False)
-                    self.df_dup.to_excel(writer, index=False, sheet_name='Duplicados')
-                    Notification.info("Arquivo Salvo",f"📁 Arquivo Excel salvo: {self.no_dup_file}")
-            except Exception as e:
-                Notification.error("Error ao Salvar",f"Erro ao salvar o arquivo Excel: {e}")
-        
-        
         except Exception as e:
-            Notification.error("Error ao Salvar",f"Ocorreu um erro durante o processamento: {e}")
-            return
+            Notification.error("Error ao Salvar",f"Erro ao salvar o arquivo Excel: {e}")
