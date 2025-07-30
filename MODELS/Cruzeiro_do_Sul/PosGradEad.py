bb01e2354c40f43d15e9887c0d26a57f5ede3c55
davison queiroz <davisonvinicius@outlook.com.br>
2025-07-30 14:23:34 -0300

    refatoração da classe pos grad para automação completa

--- MODELS/Cruzeiro_do_Sul/PosGradEad.py
+++ MODELS/Cruzeiro_do_Sul/PosGradEad.py
@@ -7,13 +7,13 @@ from datetime import date
 class PosGradEadCruzeiro:
     def __init__(self,offer_file,file_campus,file_ratio_campus):
         self.headers_transform = {
-            " CÓDIGO DA IES" : "Nome da IES",  
-            "CURSO" : "Nome do Curso",
-            "METODOLOGIA" : "Modalidade",
-            "DURAÇÃO (MESES)" : "Duração do Curso",
-            "QUANTD PARCELAS" : "Quantidade de Parcelas",
-            "PREÇO PARCELAS" : "Mensalidade sem desconto",
-            "PORCENTAGEM DE DESCONTO" : "Porcentagem de desconto da bolsa (Fixo/1 º Semestre)",
+            "Nome da IES" : "CERTIFICADORA",  
+            "Nome do Curso" : "CURSO",
+            "Modalidade" : "METODOLOGIA",
+            "Duração do Curso" : "DURAÇÃO (MESES)",
+            "Quantidade de Parcelas" : "QUANTD PARCELAS",
+            "Mensalidade sem desconto" : "PREÇO PARCELAS",
+            "Porcentagem de desconto da bolsa (Fixo/1 º Semestre)" : "PORCENTAGEM DE DESCONTO"
         }   
         self.offers = offer_file
         self.campus = file_campus
@@ -24,10 +24,11 @@ class PosGradEadCruzeiro:
         self.campus = sma(self.campus).load()
         self.pole_ratio_unp = sma(self.campus_ratio,"UNIPÊ").load()
         self.pole_ratio_pos = sma(self.campus_ratio,"POSITIVO").load()
-
+        self.pole_ratio_unp['COD_POLO'] = self.pole_ratio_unp['COD_POLO'].astype(str)
+        self.pole_ratio_pos['COD_POLO'] = self.pole_ratio_pos['COD_POLO'].astype(str)
+        
     def _adjusts_in_msp(self):
         self.msp_columns_adjusts()
-        self.transform_and_subtract_value()
         self.separe_universities_courses()
 
     def _adjusts_in_campus(self):
@@ -48,26 +49,25 @@ class PosGradEadCruzeiro:
         self.offers['Grau'] = "Especialização (pós-graduação)"
         self.offers['Qual valor usar?\n% ou R$'] = "porcentagem"
         self.offers['LIMTADA?'] = "FALSE"
-        self.offers['Data de Início da Oferta?'] = self._get_date_actually()
-        self.offers['Porcentagem de desconto IES'] = self._transform_and_subtract_value(self.offers,'Porcentagem de desconto da bolsa (Fixo/1 º Semestre)')
-        self.offers['ID da IES'] = dfu.find_and_replace(self.offers,'Nome da IES','CRUZEIRO DO SUL - PÓS EAD','ID da IES',3719)
-        self.offers['ID da IES'] = dfu.find_and_replace(self.offers,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID da IES',1639)
-        self.offers['ID da IES'] = dfu.find_and_replace(self.offers,'Nome da IES','UNIPÊ - PÓS-GRADUAÇÃO EAD','ID da IES',1593)
-        #TODO: Pegar data end, OSC e semestre de ingresso por formulário
+        self.offers.loc[:, 'Data de Início da Oferta'] = self.get_date_actually()
+        self.offers['Porcentagem de desconto IES'] = self.transform_and_subtract_value(self.offers,'Porcentagem de desconto da bolsa (Fixo/1 º Semestre)')
+        self.offers = dfu.find_and_replace(self.offers,'Nome da IES','CRUZEIRO DO SUL - PÓS EAD','ID da IES',3719)
+        self.offers = dfu.find_and_replace(self.offers,'Nome da IES','POSITIVO - PÓS-GRADUAÇÃO EAD','ID da IES',1639)
+        self.offers = dfu.find_and_replace(self.offers,'Nome da IES','UNIPÊ - PÓS-GRADUAÇÃO EAD','ID da IES',1593)
+        del self.offers['Porcentagem total de desconto da bolsa\n(2º Semestre)']
 
     def get_date_actually(self):
-        today = date.now()
-        return today.strftime("%d%m%Y")
+        today = date.today()
+        return today.strftime("%d/%m/%Y")
     
-    def transform_and_subtract_value(self,dataframe,column_name):
-        dataframe[column_name] = dataframe[column_name].astype(float)
-        dataframe[column_name] = dataframe[column_name] - 0.05
-        return dataframe[column_name].round(2)
+    def transform_and_subtract_value(self, dataframe, column_name):
+        new_column = dataframe[column_name].astype(float) - 0.05
+        return new_column.round(2).astype(str)
 
     def separe_universities_courses(self):
-        self.unipe = dfu.filter_content_by_column(self.offers,'UNIPÊ - PÓS-GRADUAÇÃO EAD','ID da IES')
-        self.positivo = dfu.filter_content_by_column(self.offers,'POSITIVO - PÓS-GRADUAÇÃO EAD','ID da IES')
-        self.cruzeiro = dfu.filter_content_by_column(self.offers,'CRUZEIRO DO SUL - PÓS EAD','ID da IES')
+        self.unipe = dfu.filter_content_by_column(self.offers,'UNIPÊ - PÓS-GRADUAÇÃO EAD','Nome da IES')
+        self.positivo = dfu.filter_content_by_column(self.offers,'POSITIVO - PÓS-GRADUAÇÃO EAD','Nome da IES')
+        self.cruzeiro = dfu.filter_content_by_column(self.offers,'CRUZEIRO DO SUL - PÓS EAD','Nome da IES')
 
     def remove_campus_have_nulls(self):
         if dfu.verify_if_have_nulls(self.campus['metadata_code']):
@@ -107,7 +107,7 @@ class PosGradEadCruzeiro:
         sum_cells = 0
         self.groups = []
         init = 0
-        for row in range(keys):
+        for row in range(len(keys)):
             current_length = keys_count.iloc[row] + 1
             sum_cells += current_length
             if limit_cells < sum_cells:
@@ -135,15 +135,28 @@ class PosGradEadCruzeiro:
         range_for = len(self.groups)
         for i in range(range_for):
             self.df_temp = dfu.find_and_replace(self.cruzeiro,'Nome da IES','CRUZEIRO DO SUL - PÓS EAD',"ID do Campus",self.groups[i])
-            dfu.save_dataframe(os.path.join(file_save,f"CRUZEIRO_POS_GRAD{i}.xlsx"),self.df_temp,'Modelo Sem Parar')
-        dfu.save_dataframe(os.path.join(file_save, "UNIPE_E_POSITIVO.xlsx"),self.msp,'Modelo Sem Parar')
+            dfu.save_dataframe(self.df_temp,os.path.join(file_save,f"CRUZEIRO_POS_GRAD{i}.xlsx"),'Modelo Sem Parar')
+        dfu.save_dataframe(self.msp,os.path.join(file_save, "UNIPE_E_POSITIVO.xlsx"),'Modelo Sem Parar')
 
-    def load(self,file_to_save):
-        self._load_dataframes()
-        self._adjusts_in_msp()
-        self._adjusts_in_campus()
-        self._fill_campus_id_in_msps()
+    def load(self):  
         try:
-            self.create_files_and_save(file_to_save)
+            self._load_dataframes()
+            self._adjusts_in_msp()
+            self._adjusts_in_campus()
+            self._fill_campus_id_in_msps()     
         except Exception as e:
             print (f"Erro durante o processamento: {e}")
+
+    def set_values_missing_in_msp(self,end_date,osc,admission_semester):
+        self.cruzeiro['Data de Fim da Oferta'] = end_date
+        self.msp['Data de Fim da Oferta'] = end_date
+        self.cruzeiro['Benefício 1 (Chave OSC)'] = osc
+        self.msp['Benefício 1 (Chave OSC)'] = osc
+        self.cruzeiro['Semestre de Ingresso'] = admission_semester
+        self.msp['Semestre de Ingresso'] = admission_semester
+
+    def save(self,path_save):
+        try:
+            self.create_files_and_save(path_save)
+        except Exception as e:
+            print (f"Erro durante o salvamento dos arquivos: {e}")
