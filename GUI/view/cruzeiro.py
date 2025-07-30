a6b578982d0d482a072601ae526b52f8b2302570
davison queiroz <davisonvinicius@outlook.com.br>
2025-07-30 14:25:10 -0300

    alteração do metodo da cruzeiro pos na view

--- GUI/view/cruzeiro.py
+++ GUI/view/cruzeiro.py
@@ -1,6 +1,6 @@
 from GUI.content_area import *
 from GUI.widgets.cards import *
-from MODELS.Cruzeiro_do_Sul.pos_grad_ead import *
+from MODELS.Cruzeiro_do_Sul.PosGradEad import PosGradEadCruzeiro
 from MODELS.Cruzeiro_do_Sul.tecnico import tecnicoCruzeiro
 from GUI.widgets.notifications import *
 
@@ -22,7 +22,7 @@ class CruzeiroDoSul(ContentArea):
         self.card_pos.create_back_card("#FF7E29","Para a verificação e preenchimento correto siga as instruções: \n1. Selecione a planilha MSP. \n2. Selecione o EXP de campus.\n3.Selecione a relação de Polos. \n4.Clique em 'Gerar'\n\n*IMPORTANTE: Certifique-se de selecionar na ordem correta, conforme instruções*")
         btn2_pos = self.card_pos.create_btn("Selecione o EXP","#F5F5F5","#000000","5px","#D4D4D4",170,30)
         self.card_pos.add_component_card(btn2_pos)
-        btn3_pos = self.card_pos.create_btn("Selecione a MSP","#F5F5F5","#000000","5px","#D4D4D4",170,30)
+        btn3_pos = self.card_pos.create_btn("Selecione Planilha ofertas","#F5F5F5","#000000","5px","#D4D4D4",170,30)
         self.card_pos.add_component_card(btn3_pos)
         self.card_pos.set_action_btn("btn_generate",self.process_pos)
 
@@ -55,13 +55,22 @@ class CruzeiroDoSul(ContentArea):
                     msp = self.card_pos.paths["btn_option3"]
                     exp = self.card_pos.paths["btn_option2"]
                     relation = self.card_pos.paths["btn_option1"]
-                    pos = pos_grad_ead(msp,exp,relation)
+                    pos = PosGradEadCruzeiro(msp,exp,relation)
                     self.card_pos.set_directory("btn_generate")
                     path_save = self.card_pos.paths["save"]
-                    pos.create_files_limited(path_save)
+                    pos.load()
+                    values = Notification.get_user_input()
+                    if values:
+                        end,osc,semester = values
+                        pos.set_values_missing_in_msp(end,osc,semester)
+                        pos.save(path_save)
+                    else:
+                         Notification.error("Erro ao carregar planilha, verifique as informações passsadas e tente novamente.")
+
                     Notification.info("Operação finalizada","Planilha gerada com sucesso.")
                     self.card_pos.set_text_btns(["btn_option1","btn_option2","btn_option3"])
                 except Exception as e:
-                    Notification.error("Arquivos incorretos","Erro ao tentar gerar planilha final. Verificar arquivos selecionados e ordem de seleção")
+                    Notification.error("Erro interno", f"Erro ao tentar gerar planilha final.\nDetalhes: {e}")
+                    
             else:
                  Notification.error("Arquivos não selecionados","Necessário selecionar os arquivos para execução da operação. Tente novamente após selecioná-los")
